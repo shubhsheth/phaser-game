5e363ff6f023643a12486a1a0c23a17f793f2482
Shubh Sheth <shubhsheth8@gmail.com>
2019-10-23 17:21:28 -0500

    Updated Init

--- app.js
+++ app.js
@@ -1,48 +1,25 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: {
-                y: 200
-            }
-        }
+const gameStart = {
+    Key: 'gameStart',
+    Preload: function() {
+        //todo
     },
-    scene: {
-        preload: preload,
-        create: create
+    Create: function() {
+        //todo
+    },
+    Update: function() {
+        //todo
     }
 }
-var game = new Phaser.Game(config);
 
-function preload ()
-{
-    this.load.setBaseURL('http://labs.phaser.io');
 
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+const config = {
+    Type: Phaser.AUTO,
+    Width: 800,
+    Height: 600,
+    Parent: 'app',
+    Scene: [
+        gameStart,
+    ]
 }
 
-function create ()
-{
-    this.add.image(400, 300, 'sky');
-
-    var particles = this.add.particles('red');
-
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
-
-    var logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
-}
+const game = new Phaser.Game(config);
